aaba8e84dbd1f0fd945b60bd4d074b4f36e3ee8a
Ricardo Alcocer <alco@ricardoalcocer.com>
2014-04-24 11:44:08 -0700

    fixed so dialog can still be used on Android

--- package/commonjs/com.alcoapps.normalizations/1.0.0/com.alcoapps.normalizations.js
+++ package/commonjs/com.alcoapps.normalizations/1.0.0/com.alcoapps.normalizations.js
@@ -19,7 +19,7 @@ function OptionsMenu(opt){
             icon:  item.icon,
             showAsAction: Ti.Android.SHOW_AS_ACTION_NEVER
           });
-
+          //console.log(item.title);
           menuItem.addEventListener("click", function(e) {
             // normalize callback so it returns e.index just like iOS
             opt.callback({index:index});
@@ -33,17 +33,22 @@ function OptionsMenu(opt){
 OptionsMenu.prototype.show=function(cb){
   _that=this;
 
-  if (Ti.Platform.osname!=='android'){
-    var opts = {
-      title       :   _that.options.title,
-      options     :   _.pluck(_that.options.options, 'title'),
-      destructive :   _that.options.destructive
-    }
+  // get only the title property
+  var menuOptions=_.pluck(_that.options.options, 'title');
 
-    var dialog = Ti.UI.createOptionDialog(opts);
-    dialog.addEventListener('click',_that.options.callback);
-    dialog.show();
-  }  
+  if (Ti.Platform.osname === 'android'){
+    menuOptions.pop(); // remove the destructive
+  }
+  
+  var opts = {
+    title       :   _that.options.title,
+    options     :   menuOptions,
+    destructive :   _that.options.destructive
+  }
+
+  var dialog = Ti.UI.createOptionDialog(opts);
+  dialog.addEventListener('click',_that.options.callback);
+  dialog.show();
 }
 
 exports.OptionsMenu=OptionsMenu;
